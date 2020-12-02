9175d62d9e43d365b72e3bf73dfec3f89e4a3d0d
Valery Popov <vpopov@devexperts.com>
2020-12-03 01:40:26 +0300

    ClimbingLeaderboard

---
+++ src/problemSolving/ClimbingTheLeaderboard.kt
@@ -0,0 +1,26 @@
+package problemSolving
+
+fun climbingLeaderboard(ranked: Array<Int>, player: Array<Int>): Array<Int> {
+    var rankTemp: Int = ranked[0]
+    val rankedArray = ArrayList<Int>()
+    rankedArray.add(rankTemp)
+    for (i in 1 until ranked.size) {
+        if (rankTemp == ranked[i]) continue
+        rankTemp = ranked[i]
+        rankedArray.add(rankTemp)
+    }
+    var lastIndex = 0
+    main@for (n in player.size - 1 downTo 0) {
+        for(r in lastIndex until rankedArray.size) {
+            if (player[n] >= rankedArray[r]) {
+                player[n] = r + 1
+                lastIndex = r
+                continue@main
+            } else if (player[n] < rankedArray[rankedArray.size - 1]) {
+                player[n] = rankedArray.size + 1
+                continue@main
+            }
+        }
+    }
+    return player
+}
