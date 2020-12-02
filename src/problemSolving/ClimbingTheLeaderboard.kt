90fc8ad2169030d0937279b11d12661bb7347c14
Valery Popov <vpopov@devexperts.com>
2020-12-03 01:52:41 +0300

    ClimbingLeaderboard description added

--- src/problemSolving/ClimbingTheLeaderboard.kt
+++ src/problemSolving/ClimbingTheLeaderboard.kt
@@ -1,5 +1,28 @@
 package problemSolving
 
+/**
+ * An arcade game player wants to climb to the top of the leaderboard and track their ranking.
+ * The game uses Dense Ranking, so its leaderboard works like this:
+ * - The player with the highest score is ranked number  on the leaderboard.
+ * - Players who have equal scores receive the same ranking number, and the next player(s) receive the immediately
+ *   following ranking number.
+ *
+ *   Example
+ *   ranked = [100, 90, 90, 80]
+ *   player = [70, 80, 105]
+ *   The ranked players will have ranks 1, 2, 2, and 3, respectively. If the player's scores are 70, 80 and 105, their
+ *   rankings after each game are 4, 3 and 1. Return [4, 3, 1].
+ *
+ *   Function Description
+ *   Complete the climbingLeaderboard function in the editor below.
+ *   climbingLeaderboard has the following parameter(s):
+ *   - int ranked[n]: the leaderboard scores
+ *   - int player[m]: the player's scores
+ *
+ *   Returns
+ *   - int[m]: the player's rank after each new score
+ */
+
 fun climbingLeaderboard(ranked: Array<Int>, player: Array<Int>): Array<Int> {
     var rankTemp: Int = ranked[0]
     val rankedArray = ArrayList<Int>()
