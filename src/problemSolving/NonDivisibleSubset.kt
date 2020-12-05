444392210be23b93bc651c856ce3c18d863ad09f
Va1erii <valerapopovsakha@gmail.com>
2020-12-05 21:51:35 +0300

    Non divisible subset

---
+++ src/problemSolving/NonDivisibleSubset.kt
@@ -0,0 +1,21 @@
+package problemSolving
+
+/**
+ * Given a set of distinct integers, print the size of a maximal subset of S where the sum of any 2 numbers
+ * in S' is not evenly divisible by k.
+ * For example, the array S = [19, 10, 12, 10, 24, 25, 22] and k = 4. One of the arrays that can be created is S'[0] = [10, 12, 25].
+ * Another is S'[1] = [19, 22, 24]. After testing all permutations, the maximum length solution array has 3 elements.
+ *
+ * Function Description
+ * Complete the nonDivisibleSubset function in the editor below. It should return an integer representing
+ * the length of the longest subset of S meeting the criteria.
+ *
+ * nonDivisibleSubset has the following parameter(s):
+ * - S: an array of integers
+ * - k: an integer
+ **/
+
+
+fun nonDivisibleSubset(k: Int, s: Array<Int>): Int {
+    TODO()
+}
