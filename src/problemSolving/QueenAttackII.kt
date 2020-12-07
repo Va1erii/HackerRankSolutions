58744fd5319403f58f997dc273a515d35b0fe6d5
Va1erii <valerapopovsakha@gmail.com>
2020-12-07 20:50:35 +0300

    Queen Attack II task description

---
+++ src/problemSolving/QueenAttackII.kt
@@ -0,0 +1,18 @@
+package problemSolving
+
+/**
+ * You will be given a square chess board with one queen and a number of obstacles placed on it.
+ * Determine how many squares the queen can attack. A queen is standing on an n x n chessboard.
+ * The chess board's rows are numbered from 1 to n, going from bottom to top.
+ * Its columns are numbered from 1 to n, going from left to right.
+ * Each square is referenced by a tuple, (r, c), describing the row, r, and column, c, where the square is located.
+ * The queen is standing at position (rq, rc). In a single move, she can attack any square in any of the eight
+ * directions (left, right, up, down, and the four diagonals).
+ *
+ * Complete the queensAttack function in the editor below.
+ * It should return an integer that describes the number of squares the queen can attack.
+ */
+
+fun queensAttack(n: Int, k: Int, r_q: Int, c_q: Int, obstacles: Array<Array<Int>>): Int {
+    TODO()
+}
