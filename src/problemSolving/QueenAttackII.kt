c1bfe1d235e6fab05ae30721fb6bfaa2e377f212
Va1erii <valerapopovsakha@gmail.com>
2020-12-08 00:53:50 +0300

    Queen's Attack II - added solution O(n)

--- src/problemSolving/QueenAttackII.kt
+++ src/problemSolving/QueenAttackII.kt
@@ -13,6 +13,53 @@ package problemSolving
  * It should return an integer that describes the number of squares the queen can attack.
  */
 
-fun queensAttack(n: Int, k: Int, r_q: Int, c_q: Int, obstacles: Array<Array<Int>>): Int {
-    TODO()
+fun queensAttack(boardSize: Int, k: Int, queenX: Int, queenY: Int, obstacles: Array<Array<Int>>): Int {
+    var top = boardSize - queenY
+    var right = boardSize - queenX
+    var bottom = boardSize - (boardSize - queenY) - 1
+    var left = boardSize - (boardSize - queenX) - 1
+    var topRight = Math.min(top, right)
+    var topLeft = Math.min(top, left)
+    var bottomRight = Math.min(bottom, right)
+    var bottomLeft = Math.min(bottom, left)
+
+    obstacles.forEach {
+        val obstacleX = it[0]
+        val obstacleY = it[1]
+        if (queenX == obstacleX && obstacleY > queenY) {
+            top = Math.min(top, obstacleY - queenY - 1)
+        }
+        if (queenY == obstacleY && queenX < obstacleX) {
+            right = Math.min(right, obstacleX - queenX - 1)
+        }
+        if (queenX == obstacleX && obstacleY < queenY) {
+            bottom = Math.min(bottom, queenY - obstacleY - 1)
+        }
+        if (queenY == obstacleY && queenX > obstacleX) {
+            left = Math.min(left, queenX - obstacleX - 1)
+        }
+        if (obstacleY > queenY && obstacleX > queenX && obstacleY - queenY == obstacleX - queenX) {
+            topRight = Math.min(topRight, obstacleY - queenY - 1)
+        }
+        if (obstacleY > queenY && obstacleX < queenX && obstacleY - queenY == queenX - obstacleX) {
+            topLeft = Math.min(topLeft, obstacleY - queenY - 1)
+        }
+        if (queenY > obstacleY && obstacleX > queenX && queenY - obstacleY == obstacleX - queenX) {
+            bottomRight = Math.min(bottomRight, queenY - obstacleY - 1)
+        }
+        if (queenY > obstacleY && obstacleX < queenX && queenY - obstacleY == queenX - obstacleX) {
+            bottomLeft = Math.min(bottomLeft, queenY - obstacleY - 1)
+        }
+    }
+    return top + right + bottom + left + topRight + topLeft + bottomLeft + bottomRight
+}
+
+fun main() {
+    val n = 5
+    val k = 3
+    val r_q = 4
+    val c_q = 3
+    val obstacles: Array<Array<Int>> = arrayOf(arrayOf(5, 5), arrayOf(4, 2), arrayOf(2, 3))
+
+    println(queensAttack(n, k, r_q, c_q, obstacles))
 }
