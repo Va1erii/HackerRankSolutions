09a7fee63594fdc08ae6d9154e232b35df07627b
Va1erii <valerapopovsakha@gmail.com>
2020-12-12 15:37:00 +0300

    Organizing Containers of Balls

---
+++ src/problemSolving/OrganizingContainersOfBalls.kt
@@ -0,0 +1,9 @@
+package problemSolving
+
+/**
+ * David has several containers, each with a number of balls in it. He has just enough containers to sort each
+ * type of ball he has into its own container. David wants to sort the balls using his sort method.
+ * As an example, David has n = 2 containers and 2 different types of balls, both of which are numbered from 0 to n - 1 = 1.
+ * The distribution of ball types per container are described by an n x n matrix of integers, M(container)(type).
+ * For example, consider the following diagram for M = [[1,4], [2,3]]
+ */
