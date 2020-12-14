728c0fc8b3f310b7fb6d9a17e4187f4494b866b6
Va1erii <valerapopovsakha@gmail.com>
2020-12-14 22:55:51 +0300

    Orginizing Container of Balls

--- src/problemSolving/OrganizingContainersOfBalls.kt
+++ src/problemSolving/OrganizingContainersOfBalls.kt
@@ -1,5 +1,8 @@
 package problemSolving
 
+import java.util.*
+
+
 /**
  * David has several containers, each with a number of balls in it. He has just enough containers to sort each
  * type of ball he has into its own container. David wants to sort the balls using his sort method.
@@ -8,6 +11,23 @@ package problemSolving
  * For example, consider the following diagram for M = [[1,4], [2,3]]
  */
 
-fun organize(){
-    
+fun organizingContainers(container: Array<IntArray>): String {
+    //array to count amount of each ball, it's position is a type and value the amount
+    val ballsCount = IntArray(container[0].size)
+    //array to count how many balls are currently in each basket
+    val basketCapacity = IntArray(container[0].size)
+    //iterate through every array and then every value in it
+    for (i in container.indices) {
+        for (j in container.indices) {
+            basketCapacity[i] += container[i][j]
+            ballsCount[j] += container[i][j]
+        }
+    }
+    Arrays.sort(ballsCount)
+    Arrays.sort(basketCapacity)
+    return if (ballsCount.contentEquals(basketCapacity)) {
+        "Possible"
+    } else {
+        "Impossible"
+    }
 }
