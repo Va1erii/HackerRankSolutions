d59af2f46d0fb6c306f31be393474100f924d633
Va1erii <valerapopovsakha@gmail.com>
2020-12-16 23:52:54 +0300

    3D Surface Area

---
+++ src/problemSolving/3DSurfaceArea.kt
@@ -0,0 +1,14 @@
+package problemSolving
+
+/**
+ * Madison, is a little girl who is fond of toys. Her friend Mason works in a toy manufacturing factory.
+ * Mason has a 2D board A of size H x W with H rows and W columns. The board is divided into cells of size 1 x 1
+ * with each cell indicated by it's coordinate (i, j). The cell (i, j) has an integer Aij written on it.
+ * To create the toy Mason stacks Aij number of cubes of size 1 x 1 x 1 on the cell (i, j).
+ * Given the description of the board showing the values of Aij and that the price of the toy is equal
+ * to the 3d surface area find the price of the toy.
+ */
+
+fun surfaceArea(A: Array<Array<Int>>): Int {
+    TODO()
+}
