fc42db63fbe7f14605bce125ab4f3db3df6c8b38
Valerii Popov <valerapopovsakha@gmail.com>
2021-04-04 18:09:26 +0300

    Longest substring

--- src/problemSolving/LongestCommonSubstring.kt
+++ src/problemSolving/LongestCommonSubstring.kt
@@ -4,9 +4,9 @@ fun longestCommonSubsequence(s1: String, s2: String): Int {
     val array = Array(s1.length) { Array(s2.length) { 0 } }
     var max = 0
     var index = 0 to 0
-    for (i in 0 until s1.length) {
-        for (j in 0 until s2.length) {
-            if (s1.get(i) != s2.get(j)) {
+    for (i in s1.indices) {
+        for (j in s2.indices) {
+            if (s1[i] != s2[j]) {
                 array[i][j] = 0
             } else {
                 val prev = if (i > 0 && j > 0) array[i - 1][j - 1] else 0
@@ -22,7 +22,7 @@ fun longestCommonSubsequence(s1: String, s2: String): Int {
     var j = index.second
     val substring = StringBuilder()
     while (i >= 0 && j >= 0) {
-        substring.append(s1.get(i))
+        substring.append(s1[i])
         i--
         j--
     }
